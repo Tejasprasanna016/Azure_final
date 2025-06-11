2968fb18160ba5afbf91e3d9314db850b3536e8d
Tejas <tejasprasanna16@gmail.com>
2025-06-11 05:36:54 +0530

    azure devops

---
+++ Python/src/main/java/com/tejas/App.java
@@ -0,0 +1,13 @@
+package com.tejas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
